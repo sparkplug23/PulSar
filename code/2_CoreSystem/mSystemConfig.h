db33003988071d34ca42986380aae58e1d2eca19
Michael <35019746+sparkplug23@users.noreply.github.com>
2022-12-18 18:16:28 +0000

    new system file

--- code/2_CoreSystem/mSystemConfig.h
+++ code/2_CoreSystem/mSystemConfig.h
@@ -16,24 +16,35 @@ enum FIRMWARE_VERSION_BRANCH_TYPE_IDS{
 
 #define FIRMWARE_VERSION_TYPE     FIRMWARE_VERSION_TYPE_DEVELOPING_ID
 #define FIRMWARE_VERSION_MAJOR    0 // Reserved for webserver working, settings saving, and being able to export (as json AND bytes) that settings
-#define FIRMWARE_VERSION_MINOR    110 // Update "ChangeLogManual.md" when incrementing
-/**@@@@@ Test to be copied into changlog on new major update
- * #31 : Flashing Heating before Colorado
- * #30 : New branches that run on the surface book as remote debugger with stack trace
+
+/**@@@@@ Minor Changes
+ * #112 : After Colorado, still the lighting only branch
+ * #111 : Working on lighting as its own branch. Removing any WLED palette usage until my palatte works!
+ * #110 : Before colorado
+ */
+#define FIRMWARE_VERSION_MINOR    112 // Update "ChangeLogManual.md" when incrementing
+
+/**@@@@@ Core Changes
  * #28 : Stable boot only happens after 120 seconds now, as work around for not checking for mqtt/network valid. ie, currently if mqtt connects and some bad pointer exists then the mqtt will try to send it before it works. This will catch that and cause fastboot recovery. Recovery period will be (120 seconds * recovery counter)
  * #28 : Added multiple methods to trigger crashes on mqtt command
  * #27 : Fixed WDT to work inside OTA
  *       Set WDT to 60 seconds on esp32
  * #26 : Enabled fastboot detection, OTA fallback and WDT on ESP32 devices as default
  */
-#define FIRMWARE_VERSION_CORE     31
-/**@@@@@ Test to be copied into changlog on new major update
- * #31 : Flashing Heating before Colorado
- * #30 : New branches that run on the surface book as remote debugger with stack trace
+#define FIRMWARE_VERSION_CORE     28
+
+/**@@@@@ Module changes
+ * #33 : Fixed incorrect use of virtual/segment length, grouping of up to 200 has been tested for some effects
+ * #32 : Moved lighting branch back to desktop 
+ * #31 : Adding a palette step that splits progmem loads to only when a palette is loaded 
  * #30 : Added ultrasonic to unified sensor
  * #29 : Flashed testbed shelly2.5 to try new modeC for outside lights that simply reports switches as motion events, with relays to be remote OH controlled
  */
-#define FIRMWARE_VERSION_MODULE   31
+#define FIRMWARE_VERSION_MODULE   33
+
+
+#define FIRMWARE_BRANCH_NAME_STRING "lighting_2022" // Additions from colorado and for december 2022, to be merged into main dev branch and then deleted
+
 
 #ifndef SETTINGS_HOLDER
 #define SETTINGS_HOLDER           1//                (random(1,1000))
