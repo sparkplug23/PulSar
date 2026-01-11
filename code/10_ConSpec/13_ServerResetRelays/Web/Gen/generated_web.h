a5424ebda1b82207d7241b4a3af98a3904fa9e1f
Michael <35019746+sparkplug23@users.noreply.github.com>
2026-01-11 10:55:32 +0000

    save

--- code/10_ConSpec/13_ServerResetRelays/Web/Gen/generated_web.h
+++ code/10_ConSpec/13_ServerResetRelays/Web/Gen/generated_web.h
@@ -1,7 +1,7 @@
 /*
  * AUTO-GENERATED FILE
  *   Generator: tools/cdata_submodules.js
- *   Time:      2026-01-11T10:16:42.371Z
+ *   Time:      2026-01-11T10:53:57.221Z
  *
  * Source:
  *   code/10_ConSpec/13_ServerResetRelays/Web/Src
